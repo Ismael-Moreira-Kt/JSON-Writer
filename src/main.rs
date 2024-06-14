f49c979f31f5a3683e28c43cd82c7ce893ed5408
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-06-14 04:33:59 +0100

    feat: Add the definitions of the Paragraph and Article structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use serde::{Serialize, Deserialize};
+use serde_json;
+use std::fs::File;
+use std::io::Write;
+
+
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Paragraph {
+    name: String,
+}
+
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Article {
+    article: String,
+    author: String,
+    paragraphs: Vec<Paragraph>, // Corrected field name
 }
